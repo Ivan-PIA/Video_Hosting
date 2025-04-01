0051a058d572feee5d9682fb9eba6b0681b456e0
Ivan-PIA <wind.vanya@gmail.com>
2025-04-01 12:11:51 +0700

    add project

---
+++ run.sh
@@ -0,0 +1,15 @@
+
+
+python manage.py runserver
+
+# /video-platform
+# │── /backend  (папка бэкенда Django)
+# │   ├── backend/ (основной проект Django)
+# │   ├── users/ (приложение для аутентификации)
+# │   ├── videos/ (приложение для видео)
+# │   ├── db.sqlite3 (База данных SQLite, можно заменить на Postgres)
+# │   ├── manage.py (Команда для запуска Django)
+# │── /frontend  (папка для React)
+# │   ├── src/ (исходники фронта)
+# │   ├── package.json (Зависимости React)
+# │── venv/ (виртуальное окружение Python)
